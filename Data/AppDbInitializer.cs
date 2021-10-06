c265b05b3bc11455adfd3aa6d1a2e6d13171bb82
MichangaVictor <onyango.vic@yandex.com>
2021-10-06 15:33:30 +0300

    exceptions and error handling added new folders

---
+++ Data/AppDbInitializer.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using my_books.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_books.Data
+{
+    public class AppDbInitializer
+    {
+        public static void Seed(IApplicationBuilder applicationBuilder)
+        {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+
+                if(!context.Books.Any())
+                {
+                    context.Books.AddRange(new Book()
+                    {
+                        Title = "C# Programming language",
+                        Description = "Very popular language",
+                        IsRead = true,
+                        DateRead = DateTime.Now.AddDays(-10),
+                        Rate = 4,
+                        Genre = "Education",
+                        CoverUrl = "https://onyango.epizy.com",
+                        DateAdded = DateTime.Now
+
+                    },
+                    new Book()
+                    {
+                        Title = "Ruby language",
+                        Description = "Very Easy language",
+                        IsRead = false,
+                        DateRead = DateTime.Now.AddDays(-5),
+                        Rate = 4,
+                        Genre = "Programming",
+                        CoverUrl = "https://onyango.epizy.com",
+                        DateAdded = DateTime.Now.AddDays(-2)
+                    });
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
---
+++ Data/Services/PublishersService.cs
@@ -0,0 +1,81 @@
+﻿using my_books.Data.Models;
+using my_books.Data.ViewModels;
+using my_books.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace my_books.Data.Services
+{
+    public class PublishersService
+    {
+        private AppDbContext _context;
+        public PublishersService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public Publisher AddPublisher(PublisherVM publisher)
+        {
+            if (StringStartWithNumber(publisher.Name)) throw new PublisherNameException("Name starts with number",
+                 publisher.Name);
+
+            var _publisher = new Publisher()
+            {
+                Name = publisher.Name
+            };
+            _context.Publishers.Add(_publisher);
+            _context.SaveChanges();
+
+            return _publisher;
+        }
+        public Publisher GetPublisherById(int id) => _context.Publishers.FirstOrDefault(n => n.Id == id);
+        
+
+        public PublisherWithBooksAndAuthorsVM GetPublisherData(int publisherId)
+        {
+            var _publisherData = _context.Publishers.Where(n => n.Id == publisherId)
+                .Select(n => new PublisherWithBooksAndAuthorsVM()
+                {
+                    Name = n.Name,
+                    BookAuthors = n.Books.Select(n => new BookAuthorVM()
+                    {
+                        BookName = n.Title,
+                        BookAuthors = n.Book_Authors.Select(n => n.Author.FullName).ToList()
+                    }).ToList()
+                }).FirstOrDefault();
+            return _publisherData;
+        }
+
+        public List<Publisher> GetAllPublishers() => _context.Publishers.ToList();
+
+        //public Publisher GetPublisherById(int publisherId) => _context.Publishers.FirstOrDefault(n => n.Id == publisherId);
+
+        public Publisher UpdatePublisherById(int publisherId, PublisherVM publisher)
+        {
+            var _publisher = _context.Publishers.FirstOrDefault(n => n.Id == publisherId);
+            if (_publisher != null)
+            {
+                _publisher.Name = publisher.Name;
+                _context.SaveChanges();
+            }
+            return _publisher;
+        }
+
+        public void DeletePublisherById(int id)
+        {
+            var _publisher = _context.Publishers.FirstOrDefault(n => n.Id == id);
+            if (_publisher != null)
+            {
+                _context.Publishers.Remove(_publisher);
+                _context.SaveChanges();
+            }
+            else
+            {
+                throw new Exception($"The publisher with id:{id} does not exist");
+            }
+        }
+        private bool StringStartWithNumber(string name) => (Regex.IsMatch(name, @"^\d"));       
+    }
+}
