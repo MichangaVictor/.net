5b74334c816c1331bf5a9a4c479631affda0dd31
MichangaVictor <onyango.vic@yandex.com>
2021-10-07 17:01:49 +0300

    api versioning

--- Controllers/PublishersController.cs
+++ Controllers/PublishersController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using my_books.ActionResults;
+using my_books.Data.Models;
 using my_books.Data.Services;
 using my_books.Data.ViewModels;
 using my_books.Exceptions;
@@ -15,11 +18,30 @@ namespace my_books.Controllers
     public class PublishersController : ControllerBase
     {
         private PublishersService _publishersService;
+        private readonly ILogger<PublishersController> _logger;
 
-        public PublishersController(PublishersService publishersService)
+        public PublishersController(PublishersService publishersService, ILogger<PublishersController> logger)
         {
             _publishersService = publishersService;
+            _logger = logger;
         }
+
+        [HttpGet("get-all-publishers")]
+
+        public IActionResult GetAllPublishers(string sortBy, string searchString, int pageNumber)
+        {
+            try
+            {
+                _logger.LogInformation("This is just a log in GetAllPublishers");
+                var _result = _publishersService.GetAllPublishers(sortBy, searchString, pageNumber);
+                return Ok(_result);
+            }
+            catch(Exception)
+            {
+                return BadRequest("Sorry we could not load the publishers");
+            }
+        }
+
         [HttpPost("add-publisher")]
         public IActionResult AddPublisher([FromBody] PublisherVM publisher)
         {
@@ -46,11 +68,25 @@ namespace my_books.Controllers
 
             if (_response != null)
             {
+                //var _responseObj = new CustomActionResultVM()
+                //{
+                //    Publisher = _response
+                //};
+                //return new CustomActionResult(_responseObj);
+
                 return Ok(_response);
             }
             else
             {
+                //var _responseObj = new CustomActionResultVM()
+                //{
+                //    Exception = new Exception("This is coming from Publishers controller")
+                //};
+                //return new CustomActionResult(_responseObj);
+
                 return NotFound();
+
+                
             }
         }
 
---
+++ Controllers/v1/TestController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_books.Controllers.v1
+{ 
+
+    [ApiVersion("1.0")]
+    [ApiVersion("1.2")]
+    [ApiVersion("1.8")]
+    [ApiVersion("2.0")]
+    [Route("api/[controller]")]
+    //[Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        
+        [HttpGet("get-test-data"), MapToApiVersion("1.0")]
+        public IActionResult Getv1()
+        {
+            return Ok("This is test Controller Version 1.0");
+        }
+        [HttpGet("get-test-data"), MapToApiVersion("1.2")]
+        public IActionResult Getv12()
+        {
+            return Ok("This is test Controller Version 1.2");
+        }
+        [HttpGet("get-test-data"),MapToApiVersion("1.8")]
+        public IActionResult Getv18()
+        {
+            return Ok("This is test Controller Version 1.8");
+        }
+        [HttpGet("get-test-data"), MapToApiVersion("2.0")]
+        public IActionResult Getv20()
+        {
+            return Ok("This is test Controller Version 2.0");
+        }
+    }
+}
